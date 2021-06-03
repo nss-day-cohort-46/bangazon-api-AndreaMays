e896ad79b0d340031e08598e9b716a66204e66b2
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-06-03 15:32:22 -0500

    Updated paymettype code for issue ticket #18 for pr comment

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -81,7 +81,7 @@ class Payments(ViewSet):
         """Handle GET requests to payment type resource"""
         payment_types = Payment.objects.all()
 
-        customer_id = self.request.query_params.get(request.auth.user)
+        customer_id = self.request.query_params.get(user=request.auth.user)
 
         if customer_id is not None:
             payment_types = payment_types.filter(customer_id=customer_id)
